6c30d5ceb4bc223e48c3b40c2beb8fba910b13da
Georgette Class-Peters <georgycp@udel.edu>
2020-08-05 20:34:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,9 +11,7 @@ def monopoly
 		railroads: {}
 end	
 def monopoly_with_second_tier
-			monopoly = {
-
-		railroads: {pieces: 4,names: {}, rent_in_dollars: {}}
+			monopoly = {	:railroads => {:pieces => 4,:names => {}, :rent_in_dollars => {}}
 	}
 
 end	
